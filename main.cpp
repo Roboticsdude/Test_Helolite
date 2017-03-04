46595763a28b996f6326404fee2b45f4e9b5b888
Robotics Dude <claudiocaldarescu@gmail.com>
2017-03-03 21:02:59 -0800

    Added C++ Code File
    Source Code for Helolite Project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test_Helolite
+//
+//  Created by Claudio Caldarescu on 3/3/17.
+//  Copyright © 2017 Claudio Caldarescu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
